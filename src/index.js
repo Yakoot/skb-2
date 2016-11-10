820568f6dab8755d5e9cb333a4b35e3ffb85875f
Anton Mamkin <mail@tbrd.ru>
2016-11-11 00:36:44 +0300

    2c

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+let XRegExp = require('xregexp');
 
 const app = express();
 app.use(cors());
@@ -9,35 +10,15 @@ app.get('/', (req, res) => {
   });
 });
 
-app.get('/task2a', (req, res) => {
-  const sum = (+req.query.a || 0) + (+req.query.b || 0);
-  res.send(sum.toString());
-});
-
-app.get('/task2b', (req, res) => {
-  let fio = req.query.fullname.trim().replace(/ +(?= )/g,'').toLowerCase() || '';
-  console.log(fio);
-  let fioArr = fio.split(' ');
-  fioArr = fioArr.map((str) => {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-  });
-  let error = false;
-  console.log(fioArr.length);
-  if (fioArr.length == 0 || fioArr.length > 3 || fioArr[0] == '' || /[\d_\/]/.test(fio)) {
-    error = true;
-    res.send('Invalid fullname');
-  }
-  if (fioArr.length == 3) {
-    let answ = `${fioArr[2]} ${fioArr[0].split('')[0] || ''}. ${fioArr[1].split('')[0] || ''}.`;
-    res.send(answ);
-  }
-  if (fioArr.length == 2) {
-    let answ = `${fioArr[1]} ${fioArr[0].split('')[0] || ''}.`;
-    res.send(answ);
-  }
-  if (fioArr.length == 1 && !error) {
-    let answ = fioArr[0];
-    res.send(answ);
+app.get('/task2c', (req, res) => {
+  let url = req.query.username;
+  let re = XRegExp('^(?:http(s)?:)?(?://)?(?:[^/]+/)? \n\
+                  (?<username> (@)?[\\p{L}\._]+).*$', 'x');
+  if (!re.test(url)) {
+    res.send("Invalid username");
+  } else {
+    let username = XRegExp.exec(url, re).username;
+    res.send(`${username.charAt(0) == '@' ? '' : '@'}${username}`);
   }
 });
 
